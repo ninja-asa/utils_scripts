33bee17ee4d34d154afd30eb0be2c88b3f14e410
Sofia Assis <sofia@adapttech.pt>
2019-06-07 23:39:24 +0100

    reloads .bashrc at the end of the script.

--- ReposPull/repos_update.sh
+++ ReposPull/repos_update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 YE='\e[33m'
 NC='\e[0m'
+GE='\e[32m'
 
 if [[ -z "${REPOROOT}" ]]; then
   echo -e "${GE} You do not have REPOROOT variable set. Define one.${NC}"
   read reposroot
-  cd reposroot
-  echo "REPOROOT=$reposroot" >> /etc/environment  
+  cd $reposroot
+  echo "export REPOROOT=$reposroot" >> ~/.bashrc
 else
   echo -e "${YE}Your REPOROOT is set as $REPOROOT${NC}"
   cd $REPOROOT
@@ -15,10 +16,11 @@ fi
 echo -e "Configuring git alias - update"
 echo "git config --global alias.up '!git remote update -p; git merge --ff-only" $
 git config --global alias.up '!git remote update -p; git merge --ff-only @{u}'
-echo "Going over yours repos ... "
+echo "Going over your repositories ... "
 for repos in */ ; do
     cd $repos
     echo -e "${YE}Updating $repos${NC}" 
     git up
     cd ..
 done
+exec bash --login
