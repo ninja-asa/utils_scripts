b84f7a787e47dc5f468bd3c20cf0a9217f6712b4
Sofia Assis <sofia@adapttech.pt>
2019-06-07 22:49:40 +0100

    Adding first version of git pull across several repositories.

---
+++ ReposPull/repos_update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+YE='\e[33m'
+NC='\e[0m'
+
+if [[ -z "${REPOROOT}" ]]; then
+  echo -e "${GE} You do not have REPOROOT variable set. Define one.${NC}"
+  read reposroot
+  cd reposroot
+  echo "REPOROOT=$reposroot" >> /etc/environment  
+else
+  echo -e "${YE}Your REPOROOT is set as $REPOROOT${NC}"
+  cd $REPOROOT
+fi
+ 
+echo -e "Configuring git alias - update"
+echo "git config --global alias.up '!git remote update -p; git merge --ff-only" $
+git config --global alias.up '!git remote update -p; git merge --ff-only @{u}'
+echo "Going over yours repos ... "
+for repos in */ ; do
+    cd $repos
+    echo -e "${YE}Updating $repos${NC}" 
+    git up
+    cd ..
+done
